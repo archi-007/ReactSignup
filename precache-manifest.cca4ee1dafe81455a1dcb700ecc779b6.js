ba8576d09d6762ebede9c33422ad7c6ba2ccc74c
Abha Jha <jhaabha08@gmail.com>
2020-07-06 12:20:19 +0530

    Updates

---
+++ precache-manifest.cca4ee1dafe81455a1dcb700ecc779b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bddc075a539ef2216da69cd1c957d6e",
+    "url": "/ReactSignup/index.html"
+  },
+  {
+    "revision": "e87b6f9bf737ccd964a9",
+    "url": "/ReactSignup/static/css/main.7c7ccd83.chunk.css"
+  },
+  {
+    "revision": "33ccaab517e7ee446e0e",
+    "url": "/ReactSignup/static/js/2.4f700dfc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactSignup/static/js/2.4f700dfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e87b6f9bf737ccd964a9",
+    "url": "/ReactSignup/static/js/main.8b2cc7a1.chunk.js"
+  },
+  {
+    "revision": "49c5fc9e89dc2838a827",
+    "url": "/ReactSignup/static/js/runtime-main.4f1004c5.js"
+  }
+]);
