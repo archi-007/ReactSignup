49267fcb0fc8d58d7735b11d6173a867bed882cb
Abha Jha <jhaabha08@gmail.com>
2020-07-09 22:21:17 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Signup from './components/signup/signup' 
+import {BrowserRouter,Switch,Route} from 'react-router-dom'
+import Login from './components/login/login'
+import ThankYouLayout from './components/Layout/layout';
+import CompanyList from './components/CompanyList/comapanyList'
 
 function App() {
   return (
-    <div className="App">
-      <Signup />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path="/signup" component={Signup}></Route>
+          <Route path="/login" component={Login}></Route>
+          <Route path="/thankyou" component={ThankYouLayout}></Route>
+          <Route path="/companyList" component={CompanyList}></Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
